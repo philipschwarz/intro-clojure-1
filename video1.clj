12ad027d7a906cccc31ff51ff498b659bbbe57d1
Philip Schwarz <philip.johann.schwarz@gmail.com>
2014-10-25 16:14:35 +0100

    exercise 2
    Write a function to add flour, a function to add milk, and a function
    to add butter.
    
    The functions should be named
    
    add-flour
    add-milk
    add-butter

--- video1.clj
+++ video1.clj
@@ -335,3 +335,31 @@
   'welcome)
 
 (bakery-help)
+
+(defn add-egg []
+  (grab :egg)
+  (squeeze)
+  (add-to-bowl))
+
+(defn add-sugar []
+  (grab :cup)
+  (scoop :sugar)
+  (add-to-bowl)
+  (release))
+
+(defn add-flour []
+  (grab :cup)
+  (scoop :flour)
+  (add-to-bowl)
+  (release))
+
+(defn add-milk []
+  (grab :cup)
+  (scoop :milk)
+  (add-to-bowl)
+  (release))
+
+(defn add-butter []
+  (grab :butter)
+  (add-to-bowl))
+
